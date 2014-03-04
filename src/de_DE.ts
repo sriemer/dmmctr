b2c5565f730072dd6df323121eaebfa299cde855
Sebastian Riemer <sebastian.riemer@gmx.de>
2014-03-04 22:29:49 +0100

    de_DE: update German translation
    With the new GUI element "Trigger Rate" we need to update the
    translation again.

--- src/de_DE.ts
+++ src/de_DE.ts
@@ -14,173 +14,178 @@
         <translation>Fehler</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="124"/>
+        <location filename="mainwindow.cpp" line="126"/>
         <source>&amp;File</source>
         <translation>&amp;Datei</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="125"/>
+        <location filename="mainwindow.cpp" line="127"/>
         <source>&amp;Options</source>
         <translation>&amp;Optionen</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="126"/>
+        <location filename="mainwindow.cpp" line="128"/>
         <source>&amp;Help</source>
         <translation>&amp;Hilfe</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="128"/>
+        <location filename="mainwindow.cpp" line="130"/>
         <source>&amp;Quit</source>
         <translation>&amp;Beenden</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="129"/>
+        <location filename="mainwindow.cpp" line="131"/>
         <source>CTRL+Q</source>
         <translation>CTRL+Q</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="133"/>
+        <location filename="mainwindow.cpp" line="135"/>
         <source>Export to &amp;CSV</source>
         <translation>Exportieren nach &amp;CSV</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="138"/>
+        <location filename="mainwindow.cpp" line="140"/>
         <source>Export to &amp;Excel</source>
         <translation>Exportieren nach &amp;Excel</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="147"/>
+        <location filename="mainwindow.cpp" line="149"/>
         <source>&amp;About</source>
         <translation>Ü&amp;ber</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="151"/>
+        <location filename="mainwindow.cpp" line="153"/>
         <source>About &amp;Qt</source>
         <translation>Über &amp;Qt</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="198"/>
+        <location filename="mainwindow.cpp" line="200"/>
         <source>Measurement</source>
         <translation>Messung</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="205"/>
+        <location filename="mainwindow.cpp" line="209"/>
         <source>Trigger/Samples</source>
         <translation>Auslösung/Abtastung</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="237"/>
+        <location filename="mainwindow.cpp" line="241"/>
         <source>Start</source>
         <translation>Start</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="248"/>
+        <location filename="mainwindow.cpp" line="252"/>
         <source>Stop</source>
         <translation>Stopp</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="257"/>
+        <location filename="mainwindow.cpp" line="261"/>
         <source>Function</source>
         <translation>Funktion</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="261"/>
+        <location filename="mainwindow.cpp" line="265"/>
         <source>Integration Time</source>
         <translation>Integrationszeit</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="265"/>
+        <location filename="mainwindow.cpp" line="269"/>
         <source>Autozero</source>
         <translation>Nullsetzung</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="269"/>
+        <location filename="mainwindow.cpp" line="273"/>
         <source>Trigger Source</source>
         <translation>Auslöserquelle</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="273"/>
+        <location filename="mainwindow.cpp" line="277"/>
         <source>Samples</source>
         <translation>Abtastwerte</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="286"/>
+        <location filename="mainwindow.cpp" line="282"/>
+        <source>Trigger Rate</source>
+        <translation>Auslösungs-Rate</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="294"/>
         <source>Select</source>
         <translation>Auswahl</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="291"/>
+        <location filename="mainwindow.cpp" line="299"/>
         <source>Clear</source>
         <translation>Löschen</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="311"/>
+        <location filename="mainwindow.cpp" line="319"/>
         <source>Serial Port</source>
         <translation>Serieller Port</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="314"/>
+        <location filename="mainwindow.cpp" line="322"/>
         <source>DMM Port</source>
         <translation>DMM Port</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="317"/>
+        <location filename="mainwindow.cpp" line="325"/>
         <source>Baud Rate</source>
         <translation>Baud-Rate</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="322"/>
+        <location filename="mainwindow.cpp" line="330"/>
         <source>Flow Control</source>
         <translation>Flusssteuerung</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="327"/>
+        <location filename="mainwindow.cpp" line="335"/>
         <source>Parity</source>
         <translation>Parität</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="332"/>
+        <location filename="mainwindow.cpp" line="340"/>
         <source>Data Bits</source>
         <translation>Datenbits</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="337"/>
+        <location filename="mainwindow.cpp" line="345"/>
         <source>Stop Bits</source>
         <translation>Stoppbits</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="444"/>
+        <location filename="mainwindow.cpp" line="452"/>
         <source>Application to open CSV</source>
         <translation>Anwendung zum CSV öffnen</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="457"/>
+        <location filename="mainwindow.cpp" line="465"/>
         <source>Excel Template</source>
         <translation>Excel-Vorlage</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="467"/>
+        <location filename="mainwindow.cpp" line="475"/>
         <source>About</source>
         <oldsource>About </oldsource>
         <translation>Über</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="469"/>
+        <location filename="mainwindow.cpp" line="477"/>
         <source>&lt;p&gt;Copyright &amp;copy; 2011-2014 Sebastian Riemer &amp;lt;sebastian.riemer@gmx.de&amp;gt;. All rights reserved.&lt;p&gt;License: GNU General Public License Version 3&lt;p&gt;This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.&lt;p&gt;</source>
         <translation>&lt;p&gt;Copyright &amp;copy; 2011-2014 Sebastian Riemer &amp;lt;sebastian.riemer@gmx.de&amp;gt;. Alle Rechte vorbehalten.&lt;p&gt;Lizenz: GNU General Public License Version 3&lt;p&gt;Dieses Programm wird in der Hoffnung verbreitet, dass es nützlich sein wird, jedoch OHNE JEGLICHE GARANTIE; sogar ohne die implizierte Garantie der allgemeinen GEBRAUCHSTAUGLICHKEIT oder der TAUGLICHKEIT FÜR EINEN BESTIMMTEN ZWECK. Lesen Sie die GNU General Public License für weitere  Details. Nur der englische Originaltext ist rechtskräftig.&lt;p&gt;</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="478"/>
+        <location filename="mainwindow.cpp" line="486"/>
         <source> is a small application that requests DC voltage values from a Tektronix 4050 DMM, calculates the sample interval and exports this data to CSV or Microsoft Excel in an NI LabVIEW SignalExpress compatible format.</source>
         <translation> ist eine kleine Anwendung, die Gleichspannungswerte von einem Tektronix 4050 DMM abfragt, das Abtastintervall berechnet und diese Daten nach CSV oder Microsoft Excel in einem NI LabVIEW SignalExpress kompatiblen Format exportiert.</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="528"/>
+        <location filename="mainwindow.cpp" line="536"/>
         <source>Choose application to open CSV</source>
         <translation>Wähle die Anwendung zum CSV öffnen</translation>
     </message>
     <message>
-        <location filename="mainwindow.cpp" line="552"/>
+        <location filename="mainwindow.cpp" line="560"/>
         <source>Choose template file to open</source>
         <translation>Wähle die Anwendung zum Vorlagedatei öffnen</translation>
     </message>
@@ -240,6 +245,21 @@
         <location filename="settings.cpp" line="54"/>
         <source>External</source>
         <translation>Extern</translation>
+    </message>
+    <message>
+        <location filename="settings.cpp" line="57"/>
+        <source>Slow (2.5/s)</source>
+        <translation>Langsam (2,5/s)</translation>
+    </message>
+    <message>
+        <location filename="settings.cpp" line="57"/>
+        <source>Medium (5/s)</source>
+        <translation>Mittel (5/s)</translation>
+    </message>
+    <message>
+        <location filename="settings.cpp" line="58"/>
+        <source>Fast (20/s)</source>
+        <translation>Schnell (20/s)</translation>
     </message>
 </context>
 </TS>
